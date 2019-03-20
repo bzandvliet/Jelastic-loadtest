7a54edcbef91e1ad713b353f027f2145a0e38a40
bzandvliet <48675918+bzandvliet@users.noreply.github.com>
2019-03-20 15:36:41 +0100

    Merge DB and WS index pages

--- index.php
+++ index.php
@@ -3,7 +3,13 @@ if ($_GET['run']) {
   # This code will run if ?run=true is set.
   exec("/var/www/webroot/ROOT/load.sh");
 }
+
+if ($_GET['run2']) {
+  # This code will run if ?run2=true is set.
+  exec("/var/www/webroot/ROOT/loaddb.sh");
+}
 ?>
 
 <!-- This link will add ?run=true to your URL, index.php?run=true -->
-<a href="?run=true">Click Me!</a>
+<a href="?run=true">Webserver Loadtest!</a> <br>
+<a href="?run2=true">Database Loadtest!</a>
